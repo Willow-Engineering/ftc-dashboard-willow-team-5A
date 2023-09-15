90148d0a974fd577193b83b35012e11f531e369a
Quinn <cameron_morris.stu@willowschoolnola.org>
2023-09-15 11:21:30 -0500

    Update helloworld_nico.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld_nico.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld_nico.java
@@ -2,15 +2,32 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "helloworld_nico")
-public class helloworld_nico extends OpMode {
+@TeleOp
+public class Exercises_4_6_Henry extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
-    }
-        @Override
-        public void loop() {
     }
 
+    @Override
+    public void loop(){
 
+        telemetry.addData("Left Stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+
+        double fwdSpeed = gamepad1.left_stick_y;
+        double ySpeed = gamepad1.left_stick_y;
+        double xSpeed = gamepad1.left_stick_x;
+
+        if (gamepad1.a) { //turbo
+            fwdSpeed *= 0.5;
+        }
+        if (!gamepad1.a) { //turbo
+            fwdSpeed *= 1;
+        }
+
+        if (!gamepad1.a) { //crazy mode
+            ySpeed = gamepad1.left_stick_x;
+            xSpeed = gamepad1.left_stick_y;
+        }
+    }
 }
