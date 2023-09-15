9d75d3fe349d1d14ed3b016a3678e9e9137407cc
TECNOGATO555 <nicolas_etkind.stu@willowschoolnola.org>
2023-09-15 11:19:55 -0500

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignmentNE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignmentNE.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
-public class GamepadAssignment extends OpMode {
+public class GamepadAssignmentNE extends OpMode {
     @Override
     public void init() {
     }
